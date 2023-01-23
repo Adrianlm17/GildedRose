1b5a73246160de1c2a4ef31249805cd496cfceb7
Adrián <adrianlomu2004@gmail.com>
2023-01-23 18:54:23 +0100

    enhancementGR(backstagePasses.py): Implemented BackstagePasses item class with respective methods.

---
+++ src/domain/backstagePasses.py
@@ -0,0 +1,37 @@
+from src.domain.normalItem import *
+
+
+
+class BackstagePasses(NormalItem):
+
+
+
+    def __init__(self, name, sellIn, quality):
+        
+        Item.__init__(self, name, sellIn, quality)
+
+
+
+    def updateQuality(self):
+
+        if self.sellIn <= 10:
+
+            self.setQuality(+2)
+        
+
+        elif self.sellIn <= 5:
+            
+            self.setQuality(+3)
+
+
+        elif self.sellIn < 0:
+
+            self.quality = 0
+        
+
+        else:
+
+            self.setQuality(+1)
+        
+
+        self.setSellIn()
